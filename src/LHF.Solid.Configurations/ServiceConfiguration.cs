d12ec8c33081d15b1570c1165087ac58799d69c5
Lucas Hernique <lucas.hfdeus@gmail.com>
2025-01-28 15:55:49 -0300

    Adicionando Camada de Configurações do projeto.

---
+++ src/LHF.Solid.Configurations/ServiceConfiguration.cs
@@ -0,0 +1,33 @@
+﻿using LHF.Solid.Business.Intefaces;
+using LHF.Solid.Business.Services;
+using LHF.Solid.Data.Context;
+using LHF.Solid.Data.Repository;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.OpenApi.Models;
+
+namespace LHF.Solid.Configurations
+{
+    public static class ServiceConfiguration
+    {
+        public static void ConfigureServices(this IServiceCollection services, IConfiguration configuration)
+        {
+            // Configuração do DbContext
+            services.AddDbContext<AppDbContext>(options =>
+                options.UseSqlite(configuration.GetConnectionString("DefaultConnection")));
+
+            // Registro das dependências
+            services.AddScoped<IProductRepository, ProductRepository>();
+            services.AddScoped<IProductService, ProductService>();
+
+            // Adicionar outros serviços necessários
+            services.AddControllers();
+            services.AddEndpointsApiExplorer();
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "SOLID API", Version = "v1" });
+            });
+        }
+    }
+}
